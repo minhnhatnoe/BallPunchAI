0bf9957014fb3eb91582e396f57ebdfbd8e5eb54
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-06-23 21:40:32 +0700

    Seperate video json files

--- src/Preprocess/README.md
+++ src/Preprocess/README.md
@@ -1,6 +1,2 @@
-# Guide for using opencv-process
-Step 1: Install python, and run "pip install opencv-python"
-Step 2: Create a folder in the same folder as the code named Dataset and paste videos in there
-Step 3: Create two subfolders named Punch and NotPunch
-Step 4: Run the code in the same folder as Dataset
-Step 5: Press Enter (Hit) or Space (Not hit) as images appear
+# Hướng dẫn làm video
+Bước 1: 
--- src/Preprocess/opencv-process.py
+++ src/Preprocess/opencv-process.py
@@ -4,24 +4,26 @@ import cv2
 
 dataset_path = path.realpath(path.join(path.realpath(__file__), path.pardir, "Dataset"))
 video_folder_path = path.realpath(path.join(dataset_path, "videos"))
-json_file_path = path.join(dataset_path, "split.json")
 
-data_dict = {}
-def load_data() -> None:
-    global data_dict
-    with open(json_file_path, "r") as data_file:
-        data_dict = json.load(data_file)
+def load_data(video_name: str) -> dict:
+    file_path = path.join(dataset_path, f"{video_name}.json")
+    try:
+        with open(file_path, "r") as data_file:
+            return json.load(data_file)
+    except FileNotFoundError:
+        with open(file_path, "w") as data_file:
+            data_file.write("{}")
+        return {}
 
-def flush_data() -> None:
-    with open(json_file_path, "w") as data_file:
+def flush_data(video_name: str, data_dict: dict) -> None:
+    file_path = path.join(dataset_path, f"{video_name}.json")
+    with open(file_path, "w") as data_file:
         json.dump(data_dict, data_file)
 
-load_data()
 
 process_queue = ["VID1", "VID2", "VID3", "VID4", "VID5", "VID6"]
 for video_name in process_queue:
-    if not video_name in data_dict:
-        data_dict[video_name] = {}
+    data_dict = load_data(video_name)
     
     video_path = path.join(video_folder_path, f"{video_name}.mp4")
     video_capture = cv2.VideoCapture(video_path)
@@ -29,7 +31,7 @@ for video_name in process_queue:
     cv2.namedWindow(video_name)
     cv2.moveWindow(video_name, 0, 0)
     count = 0
-    skipping = max(0, len(data_dict[video_name])-1)
+    skipping = max(0, len(data_dict)-1)
     for _ in range(skipping):
         if read_succesful == False:
             break
@@ -41,15 +43,15 @@ for video_name in process_queue:
         cv2.imshow(video_name, image_small)
         state = cv2.waitKey()
         if state == 32: # Space
-            data_dict[video_name][str(count)] = False
+            data_dict[str(count)] = False
         elif state == 13: # Enter
-            data_dict[video_name][str(count)] = True
+            data_dict[str(count)] = True
         else:
             print("Invalid key pressed. Flushing data and exiting")
-            flush_data()
+            flush_data(video_name, data_dict)
             exit(0)
         if count % 100 == 0:
-            flush_data()
+            flush_data(video_name, data_dict)
         read_succesful, image = video_capture.read()
         count += 1
     cv2.destroyWindow(video_name)
