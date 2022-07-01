b2ce0ebf5de16e36fc994de77800d0571cdba7f9
Nguyễn Minh Nhật <86871862+minhnhatnoe@users.noreply.github.com>
2022-07-01 15:42:39 +0700

    abc

--- src/opencv-process.py
+++ src/opencv-process.py
@@ -6,17 +6,17 @@ def process_video(video_name: str) -> bool:
     data_array = tools.load_data(video_name)
     video_path = tools.get_video_path(video_name)
     video_capture = cv2.VideoCapture(video_path)
-    read_succesful, image = video_capture.read()
+    read_successful, image = video_capture.read()
     cv2.namedWindow(video_name)
     cv2.moveWindow(video_name, 0, 0)
     count = 0
     for _ in data_array:
-        if read_succesful == False:
+        if read_successful == False:
             break
-        read_succesful, image = video_capture.read()
+        read_successful, image = video_capture.read()
         count += 1
 
-    while read_succesful:
+    while read_successful:
         image_small = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
         cv2.imshow(video_name, image_small)
         state = cv2.waitKey()
@@ -34,7 +34,7 @@ def process_video(video_name: str) -> bool:
             return True
         if count % 100 == 0:
             tools.flush_data(video_name, data_array)
-        read_succesful, image = video_capture.read()
+        read_successful, image = video_capture.read()
         count += 1
     cv2.destroyWindow(video_name)
     return False
